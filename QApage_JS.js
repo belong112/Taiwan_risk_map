b9a401c9204769c5027ea76dc8f2007eb21811f0
yujen.chiu <yujen.chiu@junyiacademy.org>
2020-06-15 20:11:05 +0800

    fix bug

--- QApage_JS.js
+++ QApage_JS.js
@@ -56,7 +56,7 @@ function submitanwser() {
 		document.getElementById("score").innerHTML = total_score;
 		showDiv('resultDiv');
 		showDiv('shopDiv');
-	}, 3000);
+	}, 300);
 }
 
 function handleAddionalFormSubmit() {
@@ -65,24 +65,25 @@ function handleAddionalFormSubmit() {
 	const houseCost = document.getElementById("costInput").value;
 	const areaSize = document.getElementById("areaInput").value;
 	const houseYear = document.getElementById("yearInput").value;
-	if (identity === 3) {
+	if (identity == 3) {
 		if (houseYear < 70) {
 			return;
 		}
 
-		itemList.push(1);
+		itemList.push(0);
 		if (houseCost > 50) {
-			itemList.push(2);
+			itemList.push(1);
 		}
 	} else {
+		itemList.push(2)
 		if (houseYear <= 90) {
-			itemList.push(4);
+			itemList.push(3);
 		}
 		if (identity == 2 && areaSize >= 15) {
-			itemList.push(5);
+			itemList.push(4);
 		}
 		if (houseCost > 50) {
-			itemList.push(2);
+			itemList.push(1);
 		}
 	}
 	console.log(itemList);
